56b6be1b93dc3224e1dd3e45b6eb9c4f0b673fc4
xx19898 <zomg13912111@gmail.com>
2023-06-19 18:08:52 +0300

    did exercises up to 3.6

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let phonebook = [
+  {
+    id:1,
+    name: 'Arto Hellas',
+    number: '040-12356'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id:3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id:4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
+  },
+  
+]
+
+app.get('/api/persons/:id', (req, res) => {
+    const user = getEntryById(phonebook,parseInt(req.params.id))
+    console.log({user})
+    res.send(user)
+})
+
+app.post('/api/persons',(req,res) => {
+    if(req.body.name === undefined) res.status(400).send('<h1>Can\'t add entry as it does not have name property</h1>')
+    else if(req.body.number === undefined) res.status(400).send('<h1>Can\'t add entry as it does not have number property</h1>')
+    else if(nameAlreadyInPhonebook(req.body.name,phonebook)) res.status(400).send('<h1>Can\'t add entry as there is already an entry with same name</h1>')
+    else{
+        const newUserId = Math.floor((Math.random() * 100000 + 10000))
+        const newUser = {...req.body,id:newUserId}
+        newUser.id = newUserId
+        phonebook.push(newUser)
+        res.send(newUser)
+    }
+})
+
+function nameAlreadyInPhonebook(name,phonebook){
+    return phonebook.some((entry) => entry.name === name)
+}
+
+function getEntryById(list,id){
+    return user = list.find((person) => person.id === id)
+}
+
+app.get('/info',(req,res) => {
+    res.send(`
+    <h1>Phonebook has info for ${phonebook.length} people </h1>
+    <p>${new Date()}</p>`)
+})
+
+app.delete('/api/persons/:id',(req,res) => {
+    deleteEntryById(phonebook,parseInt(req.params.id))
+    res.send(phonebook)
+})
+
+function deleteEntryById(list,id){
+    let newList = list.filter((entry) => entry.id != id)
+    console.log({newList})
+    phonebook = newList
+}
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
