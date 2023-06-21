36017431a407f14b439720fddd27f7396d73770e
xx19898 <zomg13912111@gmail.com>
2023-06-21 12:33:36 +0300

    added lint

--- index.js
+++ index.js
@@ -20,69 +20,69 @@ morgan.token('post-body', function (req, res) {
   }
 })
 morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens['post-body'](req,res)
-    ].join(' ')
-  })
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['post-body'](req,res)
+  ].join(' ')
+})
 
-  const errorHandler = (error,request,response,next) => {
-    console.error('GOT TO ERROR HANDLER')
-    if(error.name === 'CastError'){
-      return response.status(400).send({error:'malformatted id'})
-    }else if(error.name === 'Error'){
-      return response.status(400).send({error:error.message})
-    }else if(error.name === 'ValidationError'){
-      return response.status(400).send({error:error.message})
-    }
-    next()
+const errorHandler = (error,request,response,next) => {
+  console.error('GOT TO ERROR HANDLER')
+  if(error.name === 'CastError'){
+    return response.status(400).send({error:'malformatted id'})
+  }else if(error.name === 'Error'){
+    return response.status(400).send({error:error.message})
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).send({error:error.message})
   }
+  next()
+}
   
-  const unknownEndpoint = (request, response) => {
-    console.log('unknown endpoint')
-    response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (request, response) => {
+  console.log('unknown endpoint')
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.get('/api/persons/:id',async (req, res,next) => {
+  await getById(req.params.id).then(user =>  res.send(user)).catch(e => next(new Error(`Could not find user with id ${req.params.id}`)))
+})
+app.get('/api/persons',async (req,res) => {
+  const allEntries = await getAllEntries()
+  console.log({allEntries})
+  res.send(allEntries)
+})
+
+app.post('/api/persons',async (req,res,next) => {
+  console.log({data:req.body})
+  if(req.body.name == undefined || req.body.number == undefined || req.body.name.length === 0 || req.body.number.length === 0) next(new Error('Malformed request. Form missin either name or number'))
+  else{
+    const newUser = {...req.body}
+    await createNewEntry(newUser).then(() => res.send(newUser)).catch(e => next(e))
   }
-
-  app.get('/api/persons/:id',async (req, res,next) => {
-    await getById(req.params.id).then(user =>  res.send(user)).catch(e => next(new Error(`Could not find user with id ${req.params.id}`)))
 })
-  app.get('/api/persons',async (req,res) => {
-    const allEntries = await getAllEntries()
-    console.log({allEntries})
-    res.send(allEntries)
-  })
 
-  app.post('/api/persons',async (req,res,next) => {
-      console.log({data:req.body})
-      if(req.body.name == undefined || req.body.number == undefined || req.body.name.length === 0 || req.body.number.length === 0) next(new Error('Malformed request. Form missin either name or number'))
-      else{
-          const newUser = {...req.body}
-          await createNewEntry(newUser).then(() => res.send(newUser)).catch(e => next(e))
-      }
-  })
-
-  app.get('/info',async (req,res,next) => {
-    const count = await getCountOfAllEntries().catch(e => next(e))
-    res.send(`
-    <h1>Phonebook has info for ${count} people </h1>
-    <p>${new Date()}</p>`)
+app.get('/info',async (req,res,next) => {
+  const count = await getCountOfAllEntries().catch(e => next(e))
+  res.send(`
+  <h1>Phonebook has info for ${count} people </h1>
+  <p>${new Date()}</p>`)
 })
-  app.delete('/api/persons/:id',async (req,res,next) => {
-      await deleteById(req.params.id).then((x) => {
-        res.status(200).send(`Deleted entry with id of ${parseInt(req.params.id)}`)
-      }).catch(e => {
-        next(new Error(`Could not delete entry with id of ${req.params.id}`))
-      })
-  })  
-
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.delete('/api/persons/:id',async (req,res,next) => {
+  await deleteById(req.params.id).then((x) => {
+    res.status(200).send(`Deleted entry with id of ${parseInt(req.params.id)}`)
+  }).catch(e => {
+    next(new Error(`Could not delete entry with id of ${req.params.id}`))
   })
+})  
 
-  app.use(errorHandler)
-  app.use(unknownEndpoint)
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.use(errorHandler)
+app.use(unknownEndpoint)
--- mongo.js
+++ mongo.js
@@ -10,65 +10,65 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url).then((x) => console.log('CONNECTED TO DB')).catch((e) => console.log('ERROR WHILE TRYING TO CONNECT TO THE DB'))
 
 const phonebookEntrySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength:3,
-        required:true
-    },
-    number: {
-        type:String,
-        required: true,
-        validate:{
-            validator: function(v){
-                const numberPartsDividedByMinus = v.split('-')
-                if(numberPartsDividedByMinus.length != 2) throw new Error('Number of the person you wish to add to the phonebook is formatted in wrong way :/ . It should consist of two parts divided by \'-\' and be at least 8 characters long')
-                numberPartsDividedByMinus.forEach((part) => {
-                    const partIsLegitNumber = /^\d+$/.test(part)
-                    if(!partIsLegitNumber) throw new Error('The number you chose contains faulty characters. It should consist of numbers.')
-                })
-                const firstPartLength = numberPartsDividedByMinus[0].length
-                if(firstPartLength != 2 && firstPartLength != 3) throw new Error('First part of the number must contain 2 or 3 digits')
-                const secondPartLength = numberPartsDividedByMinus[1].length
-                if(firstPartLength + secondPartLength < 8) throw new Error('Number is too short. It should contain at least 8 numbers')
-                        }
-                    }
-                }
+  name: {
+    type: String,
+    minlength:3,
+    required:true
+  },
+  number: {
+    type:String,
+    required: true,
+    validate:{
+      validator: function(v){
+        const numberPartsDividedByMinus = v.split('-')
+        if(numberPartsDividedByMinus.length != 2) throw new Error('Number of the person you wish to add to the phonebook is formatted in wrong way :/ . It should consist of two parts divided by \'-\' and be at least 8 characters long')
+        numberPartsDividedByMinus.forEach((part) => {
+          const partIsLegitNumber = /^\d+$/.test(part)
+          if(!partIsLegitNumber) throw new Error('The number you chose contains faulty characters. It should consist of numbers.')
+        })
+        const firstPartLength = numberPartsDividedByMinus[0].length
+        if(firstPartLength != 2 && firstPartLength != 3) throw new Error('First part of the number must contain 2 or 3 digits')
+        const secondPartLength = numberPartsDividedByMinus[1].length
+        if(firstPartLength + secondPartLength < 8) throw new Error('Number is too short. It should contain at least 8 numbers')
+      }
+    }
+  }
 })
 
 console.log({serverstatus:mongoose.connection.readyState})
 
 const PhonebookEntry = mongoose.model('PhonebookEntry', phonebookEntrySchema)
 
- async function createNewEntry(newEntry){
-    console.log({SavingNewEntry: newEntry})
-    const entryToCreate = new PhonebookEntry({
-        name:newEntry.name,
-        number: newEntry.number
-    })
-    await entryToCreate.save().then(result => {
-        console.log('new entry saved!')
-    })
+async function createNewEntry(newEntry){
+  console.log({SavingNewEntry: newEntry})
+  const entryToCreate = new PhonebookEntry({
+    name:newEntry.name,
+    number: newEntry.number
+  })
+  await entryToCreate.save().then(result => {
+    console.log('new entry saved!')
+  })
 }
 
- async function getAllEntries(){
-    const allEntries = await PhonebookEntry.find({}).exec()
-    return allEntries
+async function getAllEntries(){
+  const allEntries = await PhonebookEntry.find({}).exec()
+  return allEntries
 }
 
- async function getById(id){
-    console.log({id:id})
-    const entry = await PhonebookEntry.findOne({'_id':id}).exec()
-    console.log({entry:entry})
-    return entry 
+async function getById(id){
+  console.log({id:id})
+  const entry = await PhonebookEntry.findOne({'_id':id}).exec()
+  console.log({entry:entry})
+  return entry 
 }
 
 async function getCountOfAllEntries(){
-    const count = await PhonebookEntry.count()
-    return count
+  const count = await PhonebookEntry.count()
+  return count
 }
 
 async function deleteById(id){
-    const entry = await PhonebookEntry.deleteOne({'_id':id}).exec()
+  const entry = await PhonebookEntry.deleteOne({'_id':id}).exec()
 }
 
 module.exports = {getAllEntries,getById,deleteById,createNewEntry,getCountOfAllEntries}
